351224bca31624452b8154e7d4c20fef547be911
phyerim <aamm2222@hanmail.net>
2023-04-21 22:53:11 +0900

    ..

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -5,7 +5,7 @@ import './App.css';
 const Avatar =  (()=> {
   return (
     <div className="App">
-        아바타
+        아바타페이지임
     </div>
   );
 })
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,11 +1,17 @@
 
 import React, {Component} from 'react';
 import './App.css';  
+import { useNavigate } from 'react-router';
 
 const Main =  (()=> {
+
+  
+  const navigate = useNavigate()
+
+
   return (
-    <div className="App">
-        dddd
+    <div className="App" onClick={()=>navigate('/avatar')}>
+        아바타
     </div>
   );
 })
