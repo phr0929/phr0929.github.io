9de27871565a714460ad6a2f38e78e1412b5705e
phyerim <aamm2222@hanmail.net>
2023-04-29 01:45:00 +0900

    ..

--- src/Animation/index.js
+++ src/Animation/index.js
@@ -3,24 +3,17 @@ import './animation.css'
 
 const Animation =  (()=> {
   return (
-    <div className="doubleportionMainBackground2" style={{color:'#ffffff',backgroundColor:'#323232'}}>
-        <h1 style={{fontSize:50,fontWeight:900}}>오늘은 왠지</h1>
-        <div style={{position:'relative',width:'100vw',display:'flex',alignItems:'center',justifyContent:'center'}}>
-            <h1 style={{position:'absolute',fontSize:50,fontWeight:900}} className='one'>치킨이</h1>
-            <h1 style={{position:'absolute',fontSize:50,fontWeight:900}} className='two'>탕수육이</h1>
-            <h1 style={{position:'absolute',fontSize:50,fontWeight:900}} className='three'>스파게티가</h1> 
-        </div> 
-        <h1 style={{fontSize:50,fontWeight:900}}>먹고싶다</h1> 
-        
-
-
-
-
-
-
-        
+    <div className="bgGradient">
+        <h1 className='textStyle'>오늘은 왠지</h1>
+        <div className='textAniBox'>
+            <h1 className='textStyle textAni1'>치킨이</h1>
+            <h1 className='textStyle textAni2'>탕수육이</h1>
+            <h1 className='textStyle textAni3'>스파게티가</h1>
+            <h1 className='textStyle textAni4'>햄버거가</h1>
+            <h1 className='textStyle textAni5'>라면이</h1>
+        </div>
+        <h1 className='textStyle'>먹고싶다</h1>
     </div>
-
   );
 })
 
