88472cd6971d6de218f21d95883ae0b548a459e5
phyerim <aamm2222@hanmail.net>
2023-05-22 13:33:53 +0900

    ..

--- src/Main.js
+++ src/Main.js
@@ -3,13 +3,15 @@ import React, {Component} from 'react';
 import './App.css';  
  
 import { useNavigate } from 'react-router';
+import { useLocation } from 'react-router-dom';
 import imgAvatar from './resource/avatar.svg';
 
 const Main =  (()=> {
 
   
   const navigate = useNavigate()
-
+  const location = useLocation();
+  
 
   return (
     <div className="App">
@@ -18,7 +20,7 @@ const Main =  (()=> {
           <div onClick={()=>navigate('/button/radio')}>라디오버튼</div>
           <div onClick={()=>navigate('/animation')}>애니메이션</div>
           <div onClick={()=>navigate('/game')}>똥피하기 게임(PC용)</div> */}
-          <div onClick={()=>window.open('https://phr0929.github.io/mbti')}>MBTI 카톡 스타일 테스트</div>
+          <div onClick={()=>window.location.href='https://phr0929.github.io/mbti'}>MBTI 카톡 스타일 테스트</div>
        </div>
         
     </div>
