859b9dd40264346603eef8a96e232976e67f4fab
phyerim <aamm2222@hanmail.net>
2023-05-22 13:20:54 +0900

    ..

--- src/Main.js
+++ src/Main.js
@@ -18,7 +18,7 @@ const Main =  (()=> {
           <div onClick={()=>navigate('/button/radio')}>라디오버튼</div>
           <div onClick={()=>navigate('/animation')}>애니메이션</div>
           <div onClick={()=>navigate('/game')}>똥피하기 게임(PC용)</div> */}
-          <div onClick={()=>navigate('/mbti')}>MBTI 카톡 스타일 테스트</div>
+          <div onClick={()=>window.open('https://phr0929.github.io/mbti')}>MBTI 카톡 스타일 테스트</div>
        </div>
         
     </div>
